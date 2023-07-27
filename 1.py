09b60664c0aaa46c9a6f57ee27af9f4fa6b23ac4
Raphael Palma <117413668+raphaelgpalma@users.noreply.github.com>
2023-07-27 01:38:22 -0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,50 @@
+import wikipedia as w
+import speech_recognition as sr
+import pyttsx3
+import sys
+import time as t
+
+keywords = ["definir", "defesa", "o que é", "o que foi", "quem é", "quem foi", "quem era", "procure por", "defina",
+            "definição de", "busque a definição", "explique a", "explique o", "defesa"]
+w.set_lang("pt-BR")
+audio = sr.Recognizer()
+machine = pyttsx3.init()
+machine.say("Olá, eu sou a ANA, sua Assistente de Navegação Autônoma")
+machine.say("como posso ajudar?")
+machine.runAndWait()
+
+
+def execute():
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            voz = audio.listen(source)
+            command = audio.recognize_google(voz, language="pt-br")
+            command = command.lower()
+            return command
+    except:
+        machine.say("Não entendi o que você disse, tente novamente por favor.")
+        machine.runAndWait()
+
+
+def evaluate(query):
+    for keyword in keywords:
+        if query.startswith(keyword):
+            return query.replace(keyword, "    ")
+
+
+while True:
+    word = execute()
+    print(word)
+    if "agora" in word:
+        machine.say("tudo bem, espero que eu seja útil posteriormente.")
+        machine.runAndWait()
+        break
+
+    print(w.summary(word))
+
+    t.sleep(5)
+    machine.say("algo mais que eu possa ajudar?")
+    machine.runAndWait()
+
+sys.exit()
